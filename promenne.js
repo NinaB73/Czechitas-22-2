b0305636b186ec55e8c3ab7b59d2ab6657e57019
NinaB73 <nina.bilovska@gmail.com>
2022-02-22 20:15:22 +0100

    1 file modified

--- promenne.js
+++ promenne.js
@@ -4,12 +4,18 @@ let jmeno = "Petra";
 let vek = 34;
 let vaha = 65;
 let vyska = 1.67;
-let BMI = 1.67 * 1.67;
+let BMI = 65 / 1.67 **2;
 
 
 console.log(jmeno);
 console.log('Jmenuji se ' + jmeno + ' a je mi'+' '+ vek);
-console.log('Vazim ' + vaha + 'kg' + ' a merim '+' '+ vyska);
+console.log('Vazim ' + vaha + ' ' + 'kg' + ' a merim '+' '+ vyska);
+console.log(BMI);
+
+
+
+
+
 
 
 
